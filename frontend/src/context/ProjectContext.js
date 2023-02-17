4f861e7500dba2c2dc121bed174c5e409f93e3e8
Sakhyat <sakhyatkabir1@gmail.com>
2023-02-17 16:31:18 -0500

    contecx api setup

---
+++ frontend/src/context/ProjectContext.js
@@ -0,0 +1,31 @@
+import { createContext, useReducer } from "react";
+
+const initialState = {
+  projects: [],
+};
+
+export const projectReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_PROJECT":
+      return {
+        projects: action.payload,
+      };
+    case "CREATE_PROJECT":
+      return {
+        projects: [action.payload, ...state.projects],
+      };
+    default:
+      return state;
+  }
+};
+
+export const ProjectContext = createContext();
+
+export const ProjectContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(projectReducer, initialState);
+  return (
+    <ProjectContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </ProjectContext.Provider>
+  );
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ProjectContext } from "./context/ProjectContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <ProjectContext.Provider>
+        <App />
+      </ProjectContext.Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
